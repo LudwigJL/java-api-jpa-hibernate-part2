9a2bb198055b5babf5c9ae9fed5c5a211dc46a17
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-09-10 15:24:24 +0200

    git push

---
+++ src/main/java/com/booleanuk/api/controller/BookController.java
@@ -0,0 +1,9 @@
+package com.booleanuk.api.controller;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class BookController {
+}
